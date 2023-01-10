56d951f7e97ba008ad889116f3d192d5cd0ef3ba
sanavalos <santiago.avalos@outlook.com>
2023-01-10 19:38:32 -0300

    feat(Characters): rendering characters' data

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCharacters } from "../actions/charactersActions";
 
 function Characters() {
-  return <div>Characters</div>;
+  const dispatch = useDispatch();
+  const { characters } = useSelector((state) => state.characters);
+
+  useEffect(() => {
+    dispatch(getCharacters());
+  }, []);
+
+  return (
+    <div>
+      {characters.map(({ url, name, gender, birth_year, homeworld }) => (
+        <div key={url}>
+          <h1>{name}</h1>
+          <h2>{gender}</h2>
+          <h2>{birth_year}</h2>
+          <h2>{homeworld}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Characters;
