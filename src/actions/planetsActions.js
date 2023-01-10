0bad3b4618c6542aaba04fd6063cb0ba6760d762
sanavalos <santiago.avalos@outlook.com>
2023-01-10 20:03:40 -0300

    fix(planetsActions): rendering world in card

--- src/actions/planetsActions.js
+++ src/actions/planetsActions.js
@@ -2,25 +2,25 @@ import axios from "axios";
 
 import { GET_PLANETS } from "../types";
 
-export function getPlanets() {
-  return async (dispatch) => {
-    try {
-      let planets = [];
-      let url = `https://swapi.dev/api/planets/`;
-      while (url) {
-        const { data } = await axios.get(url);
-        planets = planets.concat(data.results);
-        dispatch(setPlanets(planets));
-        url = data.next;
-      }
-    } catch (error) {
-      console.log(error);
+async function setPlanets() {
+  try {
+    let planets = [];
+    let url = `https://swapi.dev/api/planets/?page=1`;
+    while (url) {
+      const { data } = await axios.get(url);
+      planets = planets.concat(data.results);
+      url = data.next;
     }
-  };
+    return planets;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function setPlanets(planets) {
-  return function (dispatch) {
-    dispatch({ type: GET_PLANETS, payload: planets });
+export function getPlanets() {
+  return async function (dispatch) {
+    setPlanets().then((planets) =>
+      dispatch({ type: GET_PLANETS, payload: planets })
+    );
   };
 }
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCharacters } from "../actions/charactersActions";
+import { getPlanets } from "../actions/planetsActions";
 
 function Characters() {
   const dispatch = useDispatch();
   const { characters } = useSelector((state) => state.characters);
+  const { planets } = useSelector((state) => state.planets);
 
   useEffect(() => {
     dispatch(getCharacters());
+    dispatch(getPlanets());
   }, []);
+
+  const findHomeworld = (homeworld) => {
+    const planet = planets.find((planet) => planet.url === homeworld);
+    return planet.name;
+  };
 
   return (
     <div>
-      {characters.map(({ url, name, gender, birth_year, homeworld }) => (
+      {characters?.map(({ url, name, gender, birth_year, homeworld }) => (
         <div key={url}>
           <h1>{name}</h1>
           <h2>{gender}</h2>
           <h2>{birth_year}</h2>
-          <h2>{homeworld}</h2>
+          <h2>{planets.length > 0 && findHomeworld(homeworld)}</h2>
         </div>
       ))}
     </div>
